d0ae1e6961fc1e13c8ad13578cf1242997ce5cde
eras0r <er4s0r@gmail.com>
2013-11-04 23:35:34 +0100

    removed wrong charset

--- sql/create-schema.sql
+++ sql/create-schema.sql
@@ -3,7 +3,7 @@
 --
 CREATE TABLE IF NOT EXISTS `brand` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `country` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `name` varchar(255) CHARACTER SET NOT NULL,
+  `country` varchar(255) CHARACTER SET NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
